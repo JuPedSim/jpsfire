7bad6335fc2148bafb35cf5e0e8f47d99495c4cd
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2017-10-18 21:24:43 +0200

    Some python 3 stuff
    - read()'s mode 'U' is deprecated. Use new argument newline=None
    - get_tstop takes an additional parameter: chid. We don't need to glob
    - over fds-files. Probably, we only have one
    - Popen writes data in bytes (Python3), therefore open tickle file in
    - binary mode. (not tested)

--- A_smoke_sensor/FDS/A_smoke_sensor/0_slice2ascii.py
+++ A_smoke_sensor/FDS/A_smoke_sensor/0_slice2ascii.py
@@ -25,7 +25,6 @@ import sys
 import pickle
 import argparse
 import logging
-import glob
 
 basename = os.path.basename(__file__)
 logfile =  os.path.join(os.path.dirname(__file__), "log_%s.txt" % basename.split(".")[0])
@@ -36,17 +35,10 @@ logging.basicConfig(filename=logfile, level=logging.DEBUG,
 
 print("Running <%s>. (Logfile: <%s>)" % (__file__, logfile))
 
-def get_tstop(fds_path):
-
-    fds_file = glob.glob("%s/*.fds" %fds_path)
-    if len(fds_file) == 0:
-        logging.critical("No fds-file found in <%s>" % fds_path)
-        exit("ERROR: no fds-file")
-
-    if len(fds_file) > 1:
-        logging.warning("more than one fds-file was found. Calculate t_stop from the first file.")
-
-    with open(fds_file[0]) as f:
+def get_tstop(fds_path, chid):
+    fds_file = os.path.join(fds_path, chid+".fds")
+    logging.info("enter get_tstop with file <%s>" % fds_file)
+    with open(fds_file) as f:
         lines = f.readlines()
         for l in lines:
             if l.startswith("&TIME"):
@@ -58,7 +50,7 @@ def get_tstop(fds_path):
                 t_stop = float(time_line[1])
             else:
                 logging.critical("could not read t_stop from file <%s>" % fds_file[0])
-                exit("ERROR: can not read time from fds-file")
+                sys.exit("ERROR: can not read time from fds-file")
     return t_stop
 
 def getParserArgs():
@@ -119,14 +111,14 @@ t_stop=120 # todo: read from fds file. Set as default. argument parsen
 t_step=20
 t_window=1            # interpolation duration prompted by fds2ascii
 
-t_stop = get_tstop(fds_path)
+t_stop = get_tstop(fds_path, chid)
 #===============================================================================
 
 logging.info("t_stop: %.2f", t_stop)
 # BEGINNING OF THE AUTOMATIC PART - NO CHANGES NEEDED
 if not os.path.exists(fds2ascii_path):
     logging.critical("<%s> does not exist. exits." %fds2ascii_path)
-    exit("<%s> does not exist. exits." %fds2ascii_path)
+    sys.exit("<%s> does not exist. exits." %fds2ascii_path)
 
 #===============================================================================
 # config_file_name = os.path.join("config_fds2ascii.csv")
@@ -136,7 +128,7 @@ specified_location = (location[0].upper(), location[1])
 
 t_low=np.arange(t_start,t_stop+1,t_step)
 t_up=[i+t_window for i in t_low]
-
+# t_up = t_low + t_window
 number_slices=1
 
 output=quantity.replace(' ', '_') + '_'
@@ -148,8 +140,10 @@ meshes = []
 
 #==============================================================================
 # Readout of FDS file:
+# 'U' is deprecated
+# See https://docs.python.org/3/library/functions.html#open
 
-fds = open('../'+chid+'.fds', 'U')
+fds = open('../'+chid+'.fds', newline=None)
 fds_entries = fds.readlines()
 
 for i, fds_entry in enumerate(fds_entries):
@@ -165,8 +159,8 @@ meshes = np.reshape(meshes, (-1, 6))
 
 #==============================================================================
 # Readout of SMV file:
-
-smv = open('../'+chid+'.smv', 'U')
+smv = open('../'+chid+'.smv', newline=None) # 'U' is deprecated
+logging.info('open ../'+chid+'.smv')
 smv_entries = smv.readlines()
 
 for i, smv_entry in enumerate(smv_entries):
@@ -203,9 +197,9 @@ quantity = quantity.replace(' ', '_')
 if os.path.exists(os.path.join('../0_slice2ascii')):
     shutil.rmtree(os.path.join('../0_slice2ascii'))
 
-if not os.path.exists('../0_slice2ascii'):
-    logging.info('create directory \"0_slice2ascii\"')
-    os.makedirs('../0_slice2ascii')
+
+logging.info('create directory \"0_slice2ascii\"')
+os.makedirs('../0_slice2ascii')
 
 if not os.path.exists(os.path.join('../0_slice2ascii/%s_%.2f'%(specified_location[0], specified_location[1]))):
     os.makedirs(os.path.join('../0_slice2ascii/%s_%.2f'%(specified_location[0], specified_location[1])))
@@ -239,7 +233,7 @@ for k, id_slice in enumerate(id_slices):
     #Open Config File Object
 
     try:
-        fh=open(config_file_name, 'U')
+        fh=open(config_file_name, newline=None)
     except:
         logging.info("!!! The Config File %s does not exist or the path defined in the script is incorrect."% config_file_name)
         #exit()
@@ -256,28 +250,30 @@ for k, id_slice in enumerate(id_slices):
 
         logging.info("Working Directory: %s"% os.getcwd())
         # Open fds2ascii and create a seperate output and input stream.
-            
-        proc = subprocess.Popen(os.path.join(fds2ascii_path),
-                               shell=True,
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE,
-                               )
+        proc = subprocess.Popen(fds2ascii_path,
+                                shell=True,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True
+        )
         # Loop through input fields (columns) for the line passed in
         #from the fds2ascii_Config_File.csv file.
+        logging.info("Input: %s"% " ".join(map(str, input_data[1:])))
         for field in input_data[1:]:
             if field != '':
-                logging.info("Input: %s"% field)
-                proc.stdin.write('%s\n' % field)
+                proc.stdin.write(field)
             else:
                 pass
-        proc.communicate()[0]
-
-    # Pass each line in the config file to the extraction function.
+        out =proc.communicate()[0]
+        print (out)
+    # Pa
     for data_row in config_lists:
         extract_ascii_data(data_row)
         time.sleep(0.5)
 
-f = open('A_smoke_sensor/data_slice2ascii.pckl', 'w+')
+pckl_file = os.path.join('A_smoke_sensor', 'data_slice2ascii.pckl')
+f = open(pckl_file, 'wb')
+print ((chid, quantity, specified_location, t_start, t_stop, t_step, id_meshes, jps_path, plots))
 pickle.dump((chid, quantity, specified_location, t_start, t_stop, t_step, id_meshes, jps_path, plots), f)
 f.close()
 
