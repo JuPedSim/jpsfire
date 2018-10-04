8b1e95483b8e56ae34e55a2d5ac9551298db41fd
Ben Hein <ben.hein@uni-wuppertal.de>
2018-10-04 20:07:40 +0200

    update preprocess shell

--- src/preprocess_fds.sh
+++ src/preprocess_fds.sh
@@ -8,32 +8,21 @@ chids=(
 'smoke_sensor'
 )
 
-quantities=('CO2','EXTINCTION COEFFICIENT', 'CO', 'HCN', 'TEMPERATURE')
+quantities=(
+'CO2'
+'CO'
+'HCN'
+'TEMPERATURE'
+'EXTINCTION COEFFICIENT'
+)
 
 locations=(
 'Z',2.25
 )
 #==================AUTOMATIC PART - NO CHANGES NECEASSARY=======================
 
-# cd ..
-# for chid in "${chids[@]}"
-# do
-#   echo Running fds simulation...
-#   fds $chid.fds
-#   wait
-# done
-
-# cd A_smoke_sensor
-
-#for quantity in "${quantities[@]}"
-#do
-#  echo Slicefile quantity: "$quantity"
-#  echo python3 preprocess_fds.py -q "$quantity"
-#  python3 preprocess_fds.py -q "$quantity"
-#done
-
-python3 preprocess_fds.py -q 'CO2'
-python3 preprocess_fds.py -q 'CO'
-python3 preprocess_fds.py -q 'HCN'
-python3 preprocess_fds.py -q 'TEMPERATURE'
-python3 preprocess_fds.py -q 'EXTINCTION COEFFICIENT'
+for quantity in "${quantities[@]}"
+do
+  echo Slicefile quantity: "$quantity"
+  python3 preprocess_fds.py -q "$quantity" -p
+done
